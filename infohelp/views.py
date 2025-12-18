40c872c2947baa853f1e92e9ea3aa2814f3e20fb
Antônio Antunes <antunesantonio91@gmail.com>
2025-12-18 05:02:48 -0300

    feat: refatora CRUD de curso e aula para usar CBVs

--- infohelp/views.py
+++ infohelp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Curso, Dificuldade, Categoria, Aula
 from .forms import CursoForm, DificuldadeForm, CategoriaForm
 from .forms import AulaForm
@@ -124,87 +127,105 @@ def remover_biblioteca(request, curso_id):
 
 
 
-#CRUD Cursos
-@login_required
-def criar_curso(request):
-    if request.method == 'POST':
-        form = CursoForm(request.POST, request.FILES)
-        if form.is_valid():
-            curso = form.save(commit=False)
-            #curso.usuario = request.user  # Associa o curso ao usuário logado
-            curso.save()
-            return redirect('listar_cursos')
-    else:
-        form = CursoForm()
-    return render(request, "gerencia/criar_curso.html", {'form': form})
+# CRUD Cursos (Class-Based Views)
+
+class CursoCreateView(LoginRequiredMixin, CreateView):
+    model = Curso
+    form_class = CursoForm
+    template_name = 'gerencia/criar_curso.html'
+    success_url = reverse_lazy('listar_cursos')
+
+    def form_valid(self, form):
+        # If you want to associate the curso to the logged user, uncomment:
+        # curso = form.save(commit=False)
+        # curso.usuario = self.request.user
+        # curso.save()
+        return super().form_valid(form)
 
 
-def listar_cursos(request):
-    q = request.GET.get('q', '').strip()
-    cursos = Curso.objects.all()
-    if q:
-        # filter by course title, course description, aula title or aula content
-        cursos = cursos.filter(
-            Q(titulo__icontains=q) |
-            Q(descricao__icontains=q) |
-            Q(aulas__titulo__icontains=q) |
-            Q(aulas__conteudo__icontains=q)
-        ).distinct()
-    cursos = cursos.order_by('-data_criacao')
+class CursoListView(ListView):
+    model = Curso
+    template_name = 'cursos.html'
+    context_object_name = 'cursos'
 
-    saved_course_ids = []
-    if request.user.is_authenticated:
-        saved_course_ids = list(request.user.biblioteca.values_list('curso_id', flat=True))
-    return render(request, 'cursos.html', {'cursos': cursos, 'saved_course_ids': saved_course_ids, 'q': q})
+    def get_queryset(self):
+        q = self.request.GET.get('q', '').strip()
+        qs = Curso.objects.all()
+        if q:
+            qs = qs.filter(
+                Q(titulo__icontains=q) |
+                Q(descricao__icontains=q) |
+                Q(aulas__titulo__icontains=q) |
+                Q(aulas__conteudo__icontains=q)
+            ).distinct()
+        return qs.order_by('-data_criacao')
 
-
-@login_required
-def editar_curso(request, pk):
-    curso = get_object_or_404(Curso, pk=pk)
-    if request.method == 'POST':
-        form = CursoForm(request.POST, request.FILES, instance=curso)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_cursos')
-    else:
-        form = CursoForm(instance=curso)
-    return render(request, 'gerencia/editar_curso.html', {'form': form, 'curso': curso})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        q = self.request.GET.get('q', '').strip()
+        saved_course_ids = []
+        if self.request.user.is_authenticated:
+            saved_course_ids = list(self.request.user.biblioteca.values_list('curso_id', flat=True))
+        context.update({'saved_course_ids': saved_course_ids, 'q': q})
+        return context
 
 
-@login_required 
-def deletar_curso(request, pk):
-    curso = get_object_or_404(Curso, pk=pk)
-    if request.method == 'POST':
-        curso.delete()
-        return redirect('listar_cursos')
-    return render(request, 'gerencia/deletar_curso.html', {'curso': curso})
+class CursoUpdateView(LoginRequiredMixin, UpdateView):
+    model = Curso
+    form_class = CursoForm
+    template_name = 'gerencia/editar_curso.html'
+    success_url = reverse_lazy('listar_cursos')
 
+
+class CursoDeleteView(LoginRequiredMixin, DeleteView):
+    model = Curso
+    template_name = 'gerencia/deletar_curso.html'
+    success_url = reverse_lazy('listar_cursos')
 
 
 
 
-#CRUD Aulas
-@login_required
-def criar_aula(request, curso_id):
-    curso = get_object_or_404(Curso, pk=curso_id)
-    if request.method == 'POST':
-        form = AulaForm(request.POST, request.FILES)
-        if form.is_valid():
-            aula = form.save(commit=False)
-            aula.curso = curso
-            aula.save()
-            return redirect('listar_aulas', curso_id=curso_id)
-    else:
-        form = AulaForm()
-    return render(request, 'aulas/criar_aula.html', {'form': form, 'curso': curso})
+# CRUD Aulas (Class-Based Views)
 
 
-def listar_aulas(request, curso_id):
-    curso = get_object_or_404(Curso, pk=curso_id)
-    aulas = curso.aulas.all().order_by('ordem')
-    return render(request, 'aulas/listar_aulas.html', {'curso': curso, 'aulas': aulas})
+class AulaCreateView(LoginRequiredMixin, CreateView):
+    model = Aula
+    form_class = AulaForm
+    template_name = 'aulas/criar_aula.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        self.curso = get_object_or_404(Curso, pk=kwargs.get('curso_id'))
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        form.instance.curso = self.curso
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('listar_aulas', kwargs={'curso_id': self.curso.pk})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['curso'] = self.curso
+        return context
 
 
+class AulaListView(ListView):
+    model = Aula
+    template_name = 'aulas/listar_aulas.html'
+    context_object_name = 'aulas'
+
+    def get_queryset(self):
+        self.curso = get_object_or_404(Curso, pk=self.kwargs.get('curso_id'))
+        return self.curso.aulas.all().order_by('ordem')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['curso'] = self.curso
+        return context
+
+
+# detalhe_aula permanece como função para lógica de embed de vídeo
 @login_required
 def detalhe_aula(request, curso_id, aula_id):
     curso = get_object_or_404(Curso, pk=curso_id)
@@ -237,25 +258,40 @@ def detalhe_aula(request, curso_id, aula_id):
     return render(request, 'aulas/detalhe_aula.html', {'curso': curso, 'aula': aula, 'video_embed': video_embed})
 
 
-@login_required
-def editar_aula(request, curso_id, aula_id):
-    curso = get_object_or_404(Curso, pk=curso_id)
-    aula = get_object_or_404(Aula, pk=aula_id, curso=curso)
-    if request.method == 'POST':
-        form = AulaForm(request.POST, request.FILES, instance=aula)
-        if form.is_valid():
-            form.save()
-            return redirect('listar_aulas', curso_id=curso_id)
-    else:
-        form = AulaForm(instance=aula)
-    return render(request, 'aulas/editar_aula.html', {'form': form, 'curso': curso, 'aula': aula})
+class AulaUpdateView(LoginRequiredMixin, UpdateView):
+    model = Aula
+    form_class = AulaForm
+    template_name = 'aulas/editar_aula.html'
+    pk_url_kwarg = 'aula_id'
+    context_object_name = 'aula'
+
+    def get_object(self, queryset=None):
+        curso = get_object_or_404(Curso, pk=self.kwargs.get('curso_id'))
+        return get_object_or_404(Aula, pk=self.kwargs.get('aula_id'), curso=curso)
+
+    def get_success_url(self):
+        return reverse_lazy('listar_aulas', kwargs={'curso_id': self.kwargs.get('curso_id')})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['curso'] = get_object_or_404(Curso, pk=self.kwargs.get('curso_id'))
+        return context
 
 
-@login_required
-def deletar_aula(request, curso_id, aula_id):
-    curso = get_object_or_404(Curso, pk=curso_id)
-    aula = get_object_or_404(Aula, pk=aula_id, curso=curso)
-    if request.method == 'POST':
-        aula.delete()
-        return redirect('listar_aulas', curso_id=curso_id)
-    return render(request, 'aulas/deletar_aula.html', {'curso': curso, 'aula': aula})
+class AulaDeleteView(LoginRequiredMixin, DeleteView):
+    model = Aula
+    template_name = 'aulas/deletar_aula.html'
+    pk_url_kwarg = 'aula_id'
+    context_object_name = 'aula'
+
+    def get_object(self, queryset=None):
+        curso = get_object_or_404(Curso, pk=self.kwargs.get('curso_id'))
+        return get_object_or_404(Aula, pk=self.kwargs.get('aula_id'), curso=curso)
+
+    def get_success_url(self):
+        return reverse_lazy('listar_aulas', kwargs={'curso_id': self.kwargs.get('curso_id')})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['curso'] = get_object_or_404(Curso, pk=self.kwargs.get('curso_id'))
+        return context
